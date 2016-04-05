517c82c488ea2b54f51efa14aa755c7fe1d5e6d7
Keith Pijanowski <keithpij@msn.com>
2016-04-05 10:44:53 -0400

    Initialization

---
+++ topic1-variables.py
@@ -0,0 +1,42 @@
+# This is a comment.
+
+# FIXME - Clean up the incorrect print functions.
+
+# print is a built in python function.
+# When used with a single string parameter it will print the string to the terminal window.
+print 'Hello World'
+
+# input is also a builtin python function.
+# When used with a single string parameter it will first print the text to the terminal
+# and then read text entered by the user.
+# input from
+r = raw_input('Are you ready for Python!')
+print r
+
+# For a list of all builtin python functions:
+# https://docs.python.org/3/library/functions.html
+
+# integer
+a=1
+print type(a)
+
+# long
+l=750000000000000000000000
+print type(l)
+
+
+# float
+pi=3.14
+print type(pi)
+
+# string
+message='Hello'
+print type(message)
+
+# This is also a string because of the quotes
+s='3.14'
+print type(s)
+
+# An illegal variable name
+#6packsOfBeerNeeded=10
+#print 6packsOfBeerNeeded
