6703d731621a4cfcf3b08e6635f88f5f29aa0a56
Keith Pijanowski <keithpij@msn.com>
2016-08-29 07:16:16 -0400

    Sectors and Industries
    Working on Sectors and Industries.

--- market-analytics/CompanyModels.py
+++ market-analytics/CompanyModels.py
@@ -2,77 +2,117 @@
 import os
 import glob
 import DataTools
-
+import csv
+import sys
 
 class Company:
 
-    def __init__(self, companyData):
+    def __init__(self, date, companyData):
 
-        tickerIndex = 0
-        dateIndex = 1
-        openIndex = 2
-        highIndex = 3
-        lowIndex = 4
-        closeIndex = 5
-        volumeIndex = 6
+        symbolIndex = 0
+        nameIndex = 1
+        marketCapIndex = 3
+        adrtsoIndex = 4
+        ipoYearIndex = 5
+        sectorIndex = 6
+        industryIndex = 7
 
-        symbol = tickerList[tickerIndex]
-        date = tickerList[dateIndex]
-        openPrice = tickerList[openIndex]
-        high = tickerList[highIndex]
-        low = tickerList[lowIndex]
-        close = tickerList[closeIndex]
-        volume = tickerList[volumeIndex]
+        symbol = companyData[symbolIndex]
+        name = companyData[nameIndex]
+        marketCap = companyData[marketCapIndex]
+        adrtso = companyData[adrtsoIndex]
+        ipoYear = companyData[ipoYearIndex]
+        sector = companyData[sectorIndex]
+        industry = companyData[industryIndex]
 
-        self.Symbol = symbol
-        self.Date = DataTools.toDate(date)
-        self.Open = float(openPrice)
-        self.High = float(high)
-        self.Low = float(low)
-        self.Close = float(close)
-        self.Change = self.Close - self.Open
-        self.Volume = int(volume)
+        self.Symbol = symbol.strip()
+        self.Name = name.strip()
+        self.MarketCap = DataTools.toFloat(marketCap)
+        self.ADRTSO = adrtso.strip()
+        self.IPOYear = DataTools.toInt(ipoYear)
+        self.Sector = sector.strip()
+        self.Industry = industry.strip()
+        self.Date = date
 
 
 def LoadFiles():
     # currentWorkingDir = os.getcwd()
     DATA_DIR = '/Users/keithpij/Documents'
     pricingDir = os.path.join(DATA_DIR, 'eod-data')
-    nasdaqDirSearch = os.path.join(pricingDir, 'NASDAQ*.txt')
-    nyseDirSearch = os.path.join(pricingDir, 'NYSE*.txt')
+    fileSearch = os.path.join(pricingDir, '*companylist*.csv')
 
-    # Notice how two lists can be added together.
-    nasdaqFiles = glob.glob(nasdaqDirSearch)
-    nyseFiles = glob.glob(nyseDirSearch)
-    allFiles = nasdaqFiles + nyseFiles
+    allFiles = glob.glob(fileSearch)
 
-    tickerDictionary = parseFiles(allFiles)
+    companyDictionary = parseFiles(allFiles)
 
-    return tickerDictionary
+    return companyDictionary
 
 
 def parseFiles(files):
-    tickerDictionary = dict()
+    companyCount = 0
+    companyDictionary = dict()
     for file in files:
-        #print(file)
+        a = file.split('.')
+        b = a[0]  # get rid of the file extension
+        c = b.split('-')
+        date = DataTools.toDate(c[-3] + c[-2] + c[-1])
+        print(file)
+        print(date)
 
         # Read through each line of the file.
+        lineCount = 0
         fhand = open(file)
-        for line in fhand:
+        reader = csv.reader(fhand)
+        for line in reader:
+            lineCount = lineCount + 1
 
-            line = line.strip()
-            tickerList = line.split(',')
+            # The first line contains the column headers.
+            if lineCount > 1:
+                c = Company(date, line)
+                companyDictionary[c.Symbol] = c
+                companyCount = companyCount + 1
 
-            t = Ticker(tickerList)
-            # if close > 100:
-            #    print(t.Symbol)
+    return companyDictionary, companyCount
 
-            if t.Symbol not in tickerDictionary:
-                dateDictionary = dict()
-                dateDictionary[t.Date] = t
-                tickerDictionary[t.Symbol] = dateDictionary
-            else:
-                dateDictionary = tickerDictionary[t.Symbol]
-                dateDictionary[t.Date] = t
 
-    return tickerDictionary
+def getUniqueSectorsIndustries(companyDictionary):
+    # Create the sector and industry dictionaries.
+    sectors = dict()
+    industries = dict()
+
+    for symbol in companyDictionary.keys():
+        c = companyDictionary[symbol]
+
+        if c.Sector not in sectors:
+            sectors[c.Sector] = 1
+        else:
+            sectors[c.Sector] = sectors[c.Sector] + 1
+
+        if c.Industry not in industries:
+            industries[c.Industry] = 1
+        else:
+            industries[c.Industry] = industries[c.Industry] + 1
+
+    return sectors, industries
+
+
+if __name__ == '__main__':
+    companyDictionary, count = LoadFiles()
+    print(str(count) + ' companies.')
+
+    # Get a dictionary of sectors and industries
+    sectors, industries = getUniqueSectorsIndustries(companyDictionary)
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-s':
+        print('\n\nSECTORS\n')
+        count = 0
+        for s in sectors.keys():
+            count = count + 1
+            print(str(count) + '. \t' + s + ' ' + str(sectors[s]))
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-i':
+        print('\n\nINDUSTRIES\n')
+        count = 0
+        for i in industries.keys():
+            count = count + 1
+            print(str(count) + '. \t' + i + ' ' + str(industries[i]))
