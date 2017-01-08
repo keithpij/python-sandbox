528d0861de7f171cb26b5df0138e708bf099133c
Keith Pijanowski <keithpij@msn.com>
2017-01-08 10:35:13 -0500

    Removed market-analytics
    Removed the market-analytics folder.

--- market-analytics/settings.py
+++
@@ -1,6 +0,0 @@
-'''
-Settings file.
-'''
-Portfolios = []
-Portfolios.append('/Users/keithpij/Documents/eod-data/tech-watch.txt')
-Portfolios.append('/Users/keithpij/Documents/eod-data/morgan-stanley-secular-growth-stocks.txt')
